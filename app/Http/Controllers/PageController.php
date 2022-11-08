ab260d2a80bbdf97c4ffecce660b79c555050be9
Alvaro5472 <70842502+Alvaro5472@users.noreply.github.com>
2022-11-08 23:16:46 +0700

    Add files via upload

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,7 +2,9 @@
 namespace App\Http\Controllers;
 class PageController extends Controller{
     public function index(){
-        return view('main');
+        $image = asset('img/Angry Birds Realistic.jpg');
+        $title = "Looping of An Angry Green Bird";
+        return view('main', compact('image', 'title'));
     }
     public function notFound(){
         return view('notFound');
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,11 +1,14 @@
 @extends('navbar')
 
 @section('main-content')
+<link rel="stylesheet" href="css/main.css">
 <body style="background-color: rgb(230, 230, 230);">
-    
+    <div class="title">
+        <h1>{{$title}}</h1>
+    </div>
     @for ($i=0; $i < 5; $i++)
     <div class="card text-center" style="width: 18rem; margin: 10px;">
-        <img class="card-img-top" src="{{asset('img/Angry Birds Realistic.jpg')}}" alt="Green Bird">
+        <img class="card-img-top" src="{{$image}}" alt="Green Bird">
         <div class="card-body">
             <h5 class="card-title">Green Bird</h5>
             <p class="card-text">A description of a green bird</p>
